92ea9a873f3e1211d3131eaaa8ef6ac9be0e8598
Dikshashukla1 <106049102+Dikshashukla1@users.noreply.github.com>
2023-02-28 11:09:27 +0530

    Update 01-Supplier-Insert.sql

--- ECommerce/Q2/01-Supplier-Insert.sql
+++ ECommerce/Q2/01-Supplier-Insert.sql
@@ -4,4 +4,4 @@ VALUES
 	("Appario Ltd.","Mumbai",'2589631470'),
 	("Knome products","Banglore",'9785462315'),
 	("Bansal Retails","Kochi",'8975463285'),
-	("Mittal Ltd.","Lucknow",'7898456532'),
+	("Mittal Ltd.","Lucknow",'7898456532');
